e2ff975b30299b092d31349bb8377db5f4ed7a2a
SouissiHedi <souissi.hedi@esprit.tn>
2024-06-03 17:08:42 +0100

    sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from './sidebar/sidebar.component';
+import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TablesComponent } from './tables/tables.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'tables', component: TablesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'analytics', component: AnalyticsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
